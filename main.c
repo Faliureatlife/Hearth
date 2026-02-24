0cf1f328a3a2edc4713541643071eb4c5c4afe28
Faliureatlife <mnickst@mail.fresnostate.edu>
2026-02-24 13:17:59 -0800

    base

---
+++ main.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <uv.h>
+
+#define DEFAULT_PORT 7000
+#define BACKLOG 128
+uv_loop_t* loop;
+struct sockaddr_in addr;
+
+typedef struct {
+  uv_write_t req;
+  uv_buf_t buf;
+} write_req_t;
+
+struct Users {
+  int size;
+  int cap;
+  uv_tcp_t* Ubuf;
+};
+
+
+void free_write_req(uv_write_t* req){
+  write_req_t* wr = (write_req_t*) req;
+  free(wr->buf.base);
+  free(wr);
+}
+
+//used as default read allocation, suggested_size is 65536 typically
+void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf){
+  buf->base = (char*) malloc(suggested_size);
+  buf->len = suggested_size;
+}
+
+void timer_callback(uv_timer_t* handle){
+  printf("timer time\n");
+  // uv_stop(handle->loop);
+}
+
+void on_close(uv_handle_t* handle){
+  free(handle);
+}
+
+void echo_write(uv_write_t* req, int status){
+  if (status) fprintf(stderr, "Write error %s\n",uv_strerror(status));
+  free_write_req(req);
+}
+
+void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t* buf){
+  if (nread > 0) {
+    write_req_t* req = (write_req_t*) malloc(sizeof(write_req_t));
+    req->buf = uv_buf_init(buf->base, nread);
+    uv_write((uv_write_t*) req, client, &req->buf, 1, echo_write);
+    return;
+  }
+  if (nread < 0){
+    if (nread != UV_EOF)
+      fprintf(stderr, "Read error %s\n",uv_strerror(nread));
+    uv_close((uv_handle_t*) client, on_close);
+  }
+  free(buf->base);
+}
+
+void on_new_connection(uv_stream_t *server, int status){
+  if (status < 0){
+    fprintf(stderr, "Error on connection: %s\n", uv_strerror(status));
+    return;
+  }
+  uv_tcp_t *client = (uv_tcp_t*) malloc(sizeof(uv_tcp_t));
+  uv_tcp_init(loop, client);
+  //accept the connecion, handle initialized, server + client must be same loop
+  if (uv_accept(server, (uv_stream_t*) client) == 0){
+    //read from client, to the allocation callback (arg 2) and uses the function in (arg 3)
+    uv_read_start((uv_stream_t*) client, alloc_buffer, echo_read);
+  }
+}
+
+int main(int argc, char* argv[]){
+  //this is where we will enter into the server
+  //need to create IP binds, and general logic
+  //will begin with just an echo
+  //messages formatted as MarkDown, add LaTeX support later
+  //format: Commands [0..n] | Message | \r\n 
+  // printf("libuv version %s\n",uv_version_string());
+  loop = uv_default_loop();
+  uv_tcp_t server;
+
+  uv_tcp_init(loop, &server);
+  //server created with given loop
+  uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
+  //bound to IP addr + port
+  // uv_ip6_addr("0.0.0.0", DEFAULT_PORT + 1,)
+
+  //test timer
+  uv_timer_t timer;
+  uv_timer_init(loop, &timer);
+  uv_timer_start(&timer, timer_callback, 1000, 0);
+
+  //bind the server to the address(it now 'exists')
+  uv_tcp_bind(&server, (const struct sockaddr*)&addr, 0);
+  //listen at the socket given, using server casted to generic stream, with given acceptable backlog
+  //what is done upon connection is determined by on_new_connection
+  //returns non-0 if errored
+  int r = uv_listen((uv_stream_t*) &server, BACKLOG, on_new_connection);
+  if (r) {
+    fprintf(stderr, "Listen error %s\n",uv_strerror(r));
+    return 1;
+  }
+
+  printf("Listening on %d\n",DEFAULT_PORT);
+  return uv_run(loop, UV_RUN_DEFAULT); 
+}
+  
